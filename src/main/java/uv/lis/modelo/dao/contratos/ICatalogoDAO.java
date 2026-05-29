2c48276cf1354445a697f78de15c7da99d257cda
emilioo23 <emivillalobos345@gmail.com>
2026-05-29 16:34:36 -0600

    feat: interfaz ICatalogoDAO

---
+++ src/main/java/uv/lis/modelo/dao/contratos/ICatalogoDAO.java
@@ -0,0 +1,11 @@
+package uv.lis.modelo.dao.contratos;
+
+import uv.lis.modelo.dominio.*;
+import java.util.List;
+
+public interface ICatalogoDAO {
+    List<Rol> obtenerRoles() throws Exception;
+    List<MetodoPago> obtenerMetodosPago() throws Exception;
+    List<TipoPedido> obtenerTiposPedido() throws Exception;
+    List<TipoEstatus> obtenerTiposEstatus() throws Exception;
+}

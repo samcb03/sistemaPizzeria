d91de8bd7e69d77f0a9428db350fbdb3457ea37b
emilioo23 <emivillalobos345@gmail.com>
2026-05-29 16:33:53 -0600

    feat: DTO Empleado actualizado

--- src/main/java/uv/lis/modelo/dominio/Empleado.java
+++ src/main/java/uv/lis/modelo/dominio/Empleado.java
@@ -3,29 +3,14 @@ package uv.lis.modelo.dominio;
 public class Empleado extends Usuario {
     private String username;
     private String contrasena;
-    
-    public Empleado() { }
+    private Rol rol;
 
-    public Empleado(String username, String contrasena) {
-        this.username = username;
-        this.contrasena = contrasena;
-    }
+    public Empleado() {}
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public String getContrasena() { return contrasena; }
+    public void setContrasena(String contrasena) { this.contrasena = contrasena; }
+    public Rol getRol() { return rol; }
+    public void setRol(Rol rol) { this.rol = rol; }
 }

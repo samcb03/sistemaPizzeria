0ed99a2adb12000fdddeaf93485ed1c172039a60
emilioo23 <emivillalobos345@gmail.com>
2026-05-29 16:34:33 -0600

    feat: interfaz IProductoDAO

---
+++ src/main/java/uv/lis/modelo/dao/contratos/IProductoDAO.java
@@ -0,0 +1,14 @@
+package uv.lis.modelo.dao.contratos;
+
+import uv.lis.modelo.dominio.Producto;
+import java.util.List;
+
+public interface IProductoDAO {
+    boolean registrar(Producto producto) throws Exception;
+    boolean actualizar(Producto producto) throws Exception;
+    boolean eliminarLogico(int idProducto) throws Exception;
+    List<Producto> buscarTodos() throws Exception;
+    List<Producto> buscarPorNombre(String nombre) throws Exception;
+    Producto buscarPorId(int idProducto) throws Exception;
+    boolean estaEnPedidos(int idProducto) throws Exception;
+}

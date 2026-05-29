8cd485cf389845e270677a6ab861bafad86be7b8
emilioo23 <emivillalobos345@gmail.com>
2026-05-29 16:33:54 -0600

    feat: DTO Cliente

--- src/main/java/uv/lis/modelo/dominio/Cliente.java
+++ src/main/java/uv/lis/modelo/dominio/Cliente.java
@@ -1,41 +1,16 @@
 package uv.lis.modelo.dominio;
 
 public class Cliente extends Usuario {
-    private String calleNumero;
+    private int calleNumero;
     private String colonia;
     private int codigoPostal;
 
-    public Cliente() { }
+    public Cliente() {}
 
-    public Cliente(String calleNumero, String colonia, int codigoPostal) {
-        this.calleNumero = calleNumero;
-        this.colonia = colonia;
-        this.codigoPostal = codigoPostal;
-    }
-
-    public String getCalleNumero() {
-        return calleNumero;
-    }
-
-    public void setCalleNumero(String calleNumero) {
-        this.calleNumero = calleNumero;
-    }
-
-    public String getColonia() {
-        return colonia;
-    }
-
-    public void setColonia(String colonia) {
-        this.colonia = colonia;
-    }
-
-    public int getCodigoPostal() {
-        return codigoPostal;
-    }
-
-    public void setCodigoPostal(int codigoPostal) {
-        this.codigoPostal = codigoPostal;
-    }
-
-    
+    public int getCalleNumero() { return calleNumero; }
+    public void setCalleNumero(int calleNumero) { this.calleNumero = calleNumero; }
+    public String getColonia() { return colonia; }
+    public void setColonia(String colonia) { this.colonia = colonia; }
+    public int getCodigoPostal() { return codigoPostal; }
+    public void setCodigoPostal(int codigoPostal) { this.codigoPostal = codigoPostal; }
 }

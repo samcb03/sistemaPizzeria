faa74e3438e5665b722f87e9ba9a8fd923c7d58d
emilioo23 <emivillalobos345@gmail.com>
2026-05-29 16:33:51 -0600

    feat: DTO Rol

--- src/main/java/uv/lis/modelo/dominio/Rol.java
+++ src/main/java/uv/lis/modelo/dominio/Rol.java
@@ -4,28 +4,13 @@ public class Rol {
     private int idRol;
     private String nombreRol;
 
-    public Rol() { }
+    public Rol() {}
+    public Rol(int idRol, String nombreRol) { this.idRol = idRol; this.nombreRol = nombreRol; }
 
-    public Rol(int idRol, String nombreRol) {
-        this.idRol = idRol;
-        this.nombreRol = nombreRol;
-    }
+    public int getIdRol() { return idRol; }
+    public void setIdRol(int idRol) { this.idRol = idRol; }
+    public String getNombreRol() { return nombreRol; }
+    public void setNombreRol(String nombreRol) { this.nombreRol = nombreRol; }
 
-    public int getIdRol() {
-        return idRol;
-    }
-
-    public void setIdRol(int idRol) {
-        this.idRol = idRol;
-    }
-
-    public String getNombreRol() {
-        return nombreRol;
-    }
-
-    public void setNombreRol(String nombreRol) {
-        this.nombreRol = nombreRol;
-    }
-
-    
+    @Override public String toString() { return nombreRol; }
 }
